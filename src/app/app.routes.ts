3f221654aaa59cf56911d196661aaf7718a1c10d
Esteban Valente <valente.estebann@gmail.com>
2023-12-20 12:45:06 -0300

    refactor: updated the routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,41 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './pages/home/home.component';
+import { LoginComponent } from './pages/login/login.component';
+import { RegisterComponent } from './pages/register/register.component';
+import { loggedGuard } from './guards/login.guard';
+import { PlanComponent } from './pages/plan/plan.component';
+import { SettingsComponent } from './pages/settings/settings.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', component: HomeComponent, title: 'Inicio | Conversor' },
+  { path: 'home', component: HomeComponent, title: 'Inicio | Conversor' },
+  {
+    path: 'login',
+    pathMatch: 'full',
+    component: LoginComponent,
+    title: 'Iniciar sesión | Conversor',
+    //canActivate: [loggedGuard],
+  },
+  {
+    path: 'register',
+    pathMatch: 'full',
+    component: RegisterComponent,
+    title: 'Registrarse | Conversor',
+    //canActivate: [notLoggedGuard],
+  },
+  {
+    path: 'planes',
+    pathMatch: 'full',
+    component: PlanComponent,
+    title: 'Planes | Conversor',
+    //canActivate: [notLoggedGuard],
+  },
+  {
+    path: 'configuracion',
+    pathMatch: 'full',
+    component: SettingsComponent,
+    title: 'Configuracion | Conversor',
+    canActivate: [loggedGuard],
+  },
+
+];
