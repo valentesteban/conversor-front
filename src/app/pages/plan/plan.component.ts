5c2935e06301bcd6398acd2e3e08e20e8cba840e
Esteban Valente <valente.estebann@gmail.com>
2023-12-20 12:43:10 -0300

    feat: created the settings plan component

---
+++ src/app/pages/plan/plan.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-plan',
+  standalone: true,
+  imports: [],
+  templateUrl: './plan.component.html',
+})
+export class PlanComponent {
+
+}
